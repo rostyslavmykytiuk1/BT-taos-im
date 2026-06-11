760bd471c12d4da357784b5ba1b041cfb4458c88
rostyslavmykytiuk1 <rostyslavmykytiuk@ukr.net>
2026-06-11 08:31:02 +0000

    Fix the parameters of sl_bps, min_order_size, etc (restarted all)

--- agents/RebateScalperAgent.py
+++ agents/RebateScalperAgent.py
@@ -24,13 +24,13 @@ from taos.im.protocol.models import LoanSettlementOption, OrderCurrency, OrderDi
 
 _NS = 1_000_000_000
 
-MIN_ORDER_SIZE = 0.25
+MIN_ORDER_SIZE = 0.255
 
 # Hold / exit (from uid 215 RT study: tail losses at ~6s hold dominate LPM3; 6bps SL was noise)
 MIN_HOLD_S = 1.5
-MAX_HOLD_S = 3.0
-MIN_GROSS_TP_BPS = 1.5
-MAX_GROSS_SL_BPS = 30.0
+MAX_HOLD_S = 5.0
+MIN_GROSS_TP_BPS = 2.5
+MAX_GROSS_SL_BPS = 4.0
 
 MIN_REOPEN_GAP_S = 4.0
 
